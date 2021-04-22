5cefac4ec450ea2b0bba0d41977ef6439e1dc13d
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-04-22 15:52:19 -0300

    init sections home episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,48 @@
 import { GetStaticProps } from "next"
 import { format, parseISO } from 'date-fns'
 import { api } from "../services/api"
-import { ptBR } from "date-fns/locale/pt-BR"
+import ptBR from "date-fns/locale/pt-BR"
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString"
+import styles from './home.module.scss'
 
 type Episode = {
-  
+  id: String,
+  title: String,
+  thumbnail: String,
+  description: String,
+  members: String,
+  duration: Number,
+  durationAsString: String,
+  url: String,
+  publishedAt: String,
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
 
   return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Ultimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <a href="">{episode.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.AllEpisodes}>
+
+      </section>
     </div>
   )
 }
@@ -40,15 +66,20 @@ export const getStaticProps: GetStaticProps = async () => {
       members: episode.members,
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration:Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
       description: episode.description,
       url: episode.file.url,
       
     };
   })
 
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
   return{
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8, // atualização do projeto (static) feita de 8h em 8h
   }
